6068a10d92696d16fb2afc53dcbf6e41d04ab870
Anastasia S <aparicion@mail.ru>
2023-12-16 16:19:32 +0300

    Изменены метод удаления пользователей и перенесен метод createUser

---
+++ src/notebook/Main.java
@@ -0,0 +1,29 @@
+/*
+1.Научить приложение обрабатывать команду READ_ALL - чтение списка
+2. Доработать метод UPDATE - обновить только то, что введено непустым
+3. валидировать имя и фамилию на содержание пробела  и на пустое значение,
+не пустой id(чтобы нельзя было ввестипустой id, ФИО  c пробелами)
+
+ */
+package notebook;
+
+import notebook.controller.UserController;
+import notebook.model.dao.impl.FileOperation;
+import notebook.model.repository.GBRepository;
+import notebook.model.repository.impl.UserRepository;
+import notebook.view.UserView;
+
+import static notebook.util.DBConnector.DB_PATH;
+import static notebook.util.DBConnector.createDB;
+
+public class Main {
+    public static void main(String[] args) {
+        createDB();
+        FileOperation fileOperation = new FileOperation(DB_PATH);
+        GBRepository repository = new UserRepository(fileOperation);
+        UserController controller = new UserController(repository);
+        UserView view = new UserView(controller);
+        view.run();
+
+    }
+}
---
+++ src/notebook/model/repository/impl/UserRepository.java
@@ -0,0 +1,94 @@
+package notebook.model.repository.impl;
+
+import notebook.model.dao.impl.FileOperation;
+import notebook.util.mapper.impl.UserMapper;
+import notebook.model.User;
+import notebook.model.repository.GBRepository;
+
+import java.beans.PropertyEditorManager;
+import java.util.*;
+
+public class UserRepository implements GBRepository {
+    private final UserMapper mapper;
+    private final FileOperation operation;
+
+    public UserRepository(FileOperation operation) {
+        this.mapper = new UserMapper();
+        this.operation = operation;
+    }
+
+    @Override
+    public List<User> findAll() {
+        List<String> lines = operation.readAll();
+        List<User> users = new ArrayList<>();
+        for (String line : lines) {
+            users.add(mapper.toOutput(line));
+        }
+        return users;
+    }
+
+    @Override
+    public User create(User user) {
+        List<User> users = findAll();
+        long max = 0L;
+        for (User u : users) {
+            long id = u.getId();
+            if (max < id){
+                max = id;
+            }
+        }
+        long next = max + 1;
+        user.setId(next);
+        users.add(user);
+        write(users);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> update(Long userId, User update) {
+        List<User> users = findAll();
+        User editUser = users.stream()
+                .filter(u -> u.getId()
+                        .equals(userId))
+                .findFirst().orElseThrow(() -> new RuntimeException("User not found"));
+        editUser.setFirstName(update.getFirstName().isEmpty() ? editUser.getFirstName() : update.getFirstName());
+        editUser.setLastName(update.getLastName().isEmpty() ? editUser.getLastName() : update.getLastName());
+        editUser.setPhone(update.getPhone().isEmpty() ? editUser.getPhone() : update.getPhone());
+        write(users);
+        return Optional.of(update);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        List<User> users = findAll();
+        for (User user : users) {
+            if (Objects.equals(user.getId(), id)) {
+                users.removeAll(Collections.singleton(user));
+                System.out.println("удаление пользователя... ");
+                List<String> lines = new ArrayList<>();
+                for (User u : users) {
+                    lines.add(mapper.toInput(u));
+                }
+                operation.saveAll(lines);
+                break;
+            }
+
+        }
+        return false;
+    }
+
+    private void write(List<User> users) {
+        List<String> lines = new ArrayList<>();
+        for (User u: users) {
+            lines.add(mapper.toInput(u));
+        }
+        operation.saveAll(lines);
+    }
+
+
+}
